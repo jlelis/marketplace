5de6ea14f24397da0f4aa464fbca75ed0bf2dd5b
Jeferson Lelis <jeferson.lelis@outlook.com>
2020-06-26 00:41:15 -0300

    create class model products category store users

---
+++ app/Models/Product.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    public function store()
+    {
+        return $this->belongsTo(Store::class);
+    }
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+}
